66a4157bccbbc721760ebf26eb4f24b60fadc320
BISHAL DATTA <dattabishal001@gmail.com>
2024-07-03 13:06:48 +0530

    lets connect with git &  try to host

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter, Poppins, Lusitana  } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const poppins = Poppins({
+    subsets: ['latin'], display: 'swap',
+    variable: '--font-poppins',
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
